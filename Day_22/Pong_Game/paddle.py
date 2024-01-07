ecc8a0d7cfda5c5f0e7604e60988934ea4665649
Ephrem_Mekonnen <120575625+Ephrem0001@users.noreply.github.com>
2024-01-07 21:01:33 +0300

    Update paddle.py

--- Day_22/Pong_Game/paddle.py
+++ Day_22/Pong_Game/paddle.py
@@ -5,3 +5,11 @@ class Paddle:
     def __init__(self, x_value, y_value):
         self.x_value = x_value
         self.y_value = y_value
+
+    def paddle_move_direction(self):
+        paddle = Turtle()
+        paddle.shape("square")
+        paddle.color("white")
+        paddle.shapesize(stretch_wid=5, stretch_len=1)
+        paddle.penup()
+        paddle.goto(self.x_value, self.y_value)
