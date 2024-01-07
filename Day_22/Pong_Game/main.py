829941cceded699429e355bf055e73439335deb7
Ephrem_Mekonnen <120575625+Ephrem0001@users.noreply.github.com>
2024-01-07 22:17:53 +0300

    Update main.py

--- Day_22/Pong_Game/main.py
+++ Day_22/Pong_Game/main.py
@@ -1,7 +1,7 @@
+import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
-import random
 
 screen = Screen()
 screen.bgcolor("black")
@@ -12,10 +12,7 @@ screen.tracer(0)
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350, 0)
 
-x_ball = random.randint(0,350)
-y_ball = random.randint(0,350)
-
-ball_pos = Ball(x_ball, y_ball)
+ball_pos = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -24,6 +21,8 @@ screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 while game_is_on:
+    time.sleep(0.08)
     screen.update()
+    ball_pos.move()
 
 screen.exitonclick()
