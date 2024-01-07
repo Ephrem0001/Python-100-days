9bdc7e44b67f8d07638c579f070306340cad013b
Ephrem_Mekonnen <120575625+Ephrem0001@users.noreply.github.com>
2024-01-07 22:17:29 +0300

    Update ball.py

--- Day_22/Pong_Game/ball.py
+++ Day_22/Pong_Game/ball.py
@@ -2,10 +2,14 @@ from turtle import Turtle
 
 
 class Ball(Turtle):
-    def __init__(self, x, y):
+    def __init__(self):
         super().__init__()
         self.shape("circle")
         self.color("white")
-        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.shapesize(stretch_wid=1, stretch_len=1)
         self.penup()
-        self.goto(x, y)
+
+    def move(self):
+        new_x = self.xcor()+10
+        new_y = self.ycor()+10
+        self.goto(new_x, new_y)
