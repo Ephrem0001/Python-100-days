fcc0fcddefaa7dcca4520de2395e0ca64a0ca337
Ephrem_Mekonnen <120575625+Ephrem0001@users.noreply.github.com>
2024-01-09 14:50:51 +0300

    Create main.py

---
+++ Day_24/Mail_Merge/Output/main.py
@@ -0,0 +1,12 @@
+PLACEHOLDER = "[name]"
+
+with open('./Input/Names/invited_names.txt') as names_file:
+    name_content = names_file.readlines()
+
+with open('./Input/Letters/starting_letter.txt') as letter_file:
+    letter_file = letter_file.read()
+    for name in name_content:
+        stripped_names = name.strip()
+        updated_letter = letter_file.replace(PLACEHOLDER, name)
+        with open(f'./Output/ReadyToSend/Letter_for_{stripped_names}.txt', 'w') as updated_letter_file:
+            updated_letter_file.write(updated_letter)
