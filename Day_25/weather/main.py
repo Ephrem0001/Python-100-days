751f273b1fd9de3c4b20d605638a1375b9100b3e
Ephrem_Mekonnen <120575625+Ephrem0001@users.noreply.github.com>
2024-01-10 10:31:57 +0300

    Update main.py

--- Day_25/weather/main.py
+++ Day_25/weather/main.py
@@ -1,10 +1,4 @@
-import csv
+import pandas
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temprature = []
-    for row in data:
-        if row[1] !="temp":
-            temprature.append(int(row[1]))
-    print(temprature)
-
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
