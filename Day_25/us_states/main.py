5233fe435b259b0b201c3fae1d1bb51c68625715
Ephrem_Mekonnen <120575625+Ephrem0001@users.noreply.github.com>
2024-01-12 23:31:57 +0300

    Update main.py

--- Day_25/us_states/main.py
+++ Day_25/us_states/main.py
@@ -14,6 +14,14 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title="Guess the state", prompt="What's state's name?")
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("States_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -24,9 +32,3 @@ while len(guessed_states) < 50:
         t.write(answer_state)
 
 screen.exitonclick()
-
-
-
-
-
-
