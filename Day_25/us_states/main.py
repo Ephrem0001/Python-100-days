a4ec08b2d43073f8a8f90c5c64287f0052017480
Ephrem_Mekonnen <120575625+Ephrem0001@users.noreply.github.com>
2024-01-12 22:28:44 +0300

    Update main.py

--- Day_25/us_states/main.py
+++ Day_25/us_states/main.py
@@ -8,6 +8,16 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_List()
 
 answer_state = screen.textinput(title="Guess the state", prompt="What's state's name")
 print(answer_state)
+
+if answer_state in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    t.goto(state_data.x, state_data.y)
+    t.write(state_data.state)
+    
